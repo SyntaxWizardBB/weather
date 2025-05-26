919b584636fbe69a2511e6a01621c856218c66b8
Lukas Brosi <lukas.brosi@edu.teko.ch>
2025-05-26 10:17:38 +0200

    Release version 2.0.0

---
+++ v2/go.mod
@@ -0,0 +1,3 @@
+module github.com/SyntaxWizardBB/weather/v2
+
+go 1.24
---
+++ v2/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"github.com/SyntaxWizardBB/weather/v2/weatherapp"
+)
+
+// Imports omitted
+func main() {
+	fmt.Printf("Weather package version: %s\n", weather.GetVersion())
+	makePrediction(51.509865, -0.118092, "London")
+}
+func makePrediction(lat float64, long float64, locationName string) error {
+	pred, err := weather.PredictAtCoords(lat, long)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("The weather prediction for %s is: %v\n", locationName,
+		pred.ToString())
+	return nil
+}
